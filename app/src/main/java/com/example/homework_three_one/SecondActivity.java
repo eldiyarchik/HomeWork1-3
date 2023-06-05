2165fc1c1132189474fae354aa789abfb1f43de0
eldiyarchik <eldiarbaurzhanov@gmail.com>
2023-06-05 15:43:07 +0600

    first

---
+++ app/src/main/java/com/example/homework_three_one/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.homework_three_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+ TextView SecondName,SecondAdres,SecondNumber;
+    String StringName,StringdAdres,StringNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        StringName = getIntent().getStringExtra("MainName");
+        StringdAdres = getIntent().getStringExtra("MainAdres");
+        StringNumber = getIntent().getStringExtra("MainNumber");
+        findViewsSecond();
+        SecondName.setText(StringName);
+        SecondAdres.setText(StringdAdres);
+        SecondNumber.setText(StringNumber);
+    }
+
+    private void findViewsSecond(){
+        SecondName = findViewById(R.id.second_Name);
+        SecondAdres = findViewById(R.id.second_Adres);
+        SecondNumber = findViewById(R.id.second_Number);
+    }
+}
